530da6e6e39027416953402a76847b4b409ab694
Sampurna KC <sampurnakc19@gmail.com>
2026-07-14 01:21:11 +0545

    Initial Commit, Setting up portfolio, only testing the placeholders

---
+++ src/data.ts
@@ -0,0 +1,54 @@
+import { AboutMeData, Game, GameEngine, LinkImageSource, MediaType, Platform } from "./types";
+
+export const PersonalInfo: AboutMeData = {
+  name: "Sampurna",
+  role: "Unity Game Developer",
+  introduction: "A game developer who is absolutely into video games.",
+  description: "TELL_ABOUT_YOURSELF",
+  image: `${process.env.PUBLIC_URL}/images/Profile.jpg`,
+  cvUri: `${process.env.PUBLIC_URL}/files/CV_Sampurna_KC.pdf`,
+  links: {
+    github: "https://github.com/TheZero19",
+    itchIO: "https://NAME.itch.io",
+    linkedIn: "https://www.linkedin.com/in/sampurna-kc-21b217282/",
+  }
+};
+
+export const games: Game[] = [
+  {
+    name: "GAME_NAME",
+    description: "GAME_DESCRIPTION",
+    genres: ["GAME_GENRE", "GAME_GENRE..."],
+    platforms: [Platform.Windows],
+    engine: GameEngine.Unity,
+    links: [
+      { source: LinkImageSource.Github, url: "https://github.com/YOUR_REPO" },
+      { source: LinkImageSource.ItchIo, url: "https://YOUR_ITCH.itch.io/GAME_NAME" },
+    ],
+    media: [
+      { source: "/images/games/GAME_NAME/Screenshot_1.png", type: MediaType.Image },
+      { source: "https://www.youtube.com/embed/dQw4w9WgXcQ", type: MediaType.YouTube },
+      { source: "/images/games/GAME_NAME/Screenshot_2.png", type: MediaType.Image },
+      { source: "/images/games/GAME_NAME/Screenshot_3.png", type: MediaType.Image },
+      { source: "/images/games/GAME_NAME/Screenshot_4.png", type: MediaType.Image },
+    ],
+  },
+  {
+    name: "GAME_NAME",
+    description: "GAME_DESCRIPTION",
+    genres: ["GAME_GENRE", "GAME_GENRE..."],
+    platforms: [Platform.Windows],
+    engine: GameEngine.Unity,
+    links: [
+      { source: LinkImageSource.Github, url: "https://github.com/YOUR_REPO" },
+      { source: LinkImageSource.ItchIo, url: "https://YOUR_ITCH.itch.io/GAME_NAME" },
+    ],
+    media: [
+      { source: "/images/games/GAME_NAME/Screenshot_1.png", type: MediaType.Image },
+      { source: "https://www.youtube.com/embed/dQw4w9WgXcQ", type: MediaType.YouTube },
+      { source: "/images/games/GAME_NAME/Screenshot_2.png", type: MediaType.Image },
+      { source: "/images/games/GAME_NAME/Screenshot_3.png", type: MediaType.Image },
+      { source: "/images/games/GAME_NAME/Screenshot_4.png", type: MediaType.Image },
+    ],
+  }
+];
